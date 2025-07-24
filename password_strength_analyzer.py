bd92c67be36e0b07fdc3ab2f2062f07dc08810f7
Jonathan C. Lindo, Jr. <jonathanlindo.jr2008@gmail.com>
2025-07-24 08:36:53 +0800

    Update password_strength_analyzer.py
    not yet finished, just saving it

--- password_strength_analyzer.py
+++ password_strength_analyzer.py
@@ -1,87 +1,74 @@
-# length, special character, uppercase lowercase, digits, (root, admin, god), entropy estimate, 
-import re
-
-# user_password = (input('Input password to test: '))
-
-# def length():
-#     length = len(user_password)
-#     if length < 8:
-#         print('short')
-#     elif length > 8:
-#         print('long')
-
-# def specialchar():
-#     specials = "!@#$%^&*()_+{}[]|\\:;\"'<>,.?/~`"
-#     for char in specials:
-#         if char in user_password:
-#             print('special present')
-#             break
-
-# specialchar()
-
-# def digits():
-#     for char in user_password:
-#         if char.isdigit():
-#             print('numbers present')
-#             break
-# digits()
-
-# def usual():
-#     key_phrases = ['admin', 'god', 'root', '123456', 'password', 'qwerty']
-#     for keys in key_phrases:
-#         if keys in user_password:
-#             print('common present')
-
-# usual()
-
-# def charcase():
-    # for letter in user_password:
-    #     if letter.islower():
-    #         print('lower')
-    #     elif letter.isupper():
-    #         print('upper')
-# charcase()
+# length, special character, uppercase lowercase, digits, (root, admin, god)
+import time
+import sys
 
 #CLEARTEXT RISK, ENCRYPTED GATE, QUANTUM VAULT
+# def welcomemessage(text, delay=0.07):
+#     for character in text:
+#         sys.stdout.write(character)
+#         sys.stdout.flush()
+#         time.sleep(delay)
+#     print()
+# welcomemess= "    Welcome to Topiary's Password Strength Analyzer    "
+# welcomemessage(welcomemess, delay=0.07)
 
-user_password = (input('Input password to test: '))
-strength_label = ['ClearText Risk', 'Encrypted Gate', 'Quantum Vault']
-cleartext_risk = []
-encrypted_gate = []
-quantum_vault = []
-def passwordstrengthcheck(password):
-    score = 0
-    # check length
-    length = len(user_password)
-    if length > 12:
-        score += 2
-    elif length >= 8 and length <= 12:
-        score += 1
-    elif length < 8:
-        score += 0
+def proceed(text, delay = 0.06):
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print
+question = input('Do you want to you to use the program? (y/n): ')
+if question == 'y':
     
-    #special characters
-    specials = "!@#$%^&*()_+{}[]|\\:;\"'<>,.?/~`"
+        user_password = (input('Input password to test: '))
+        strength_label = ['ClearText Risk', 'Encrypted Gate', 'Quantum Vault']
 
-    if any(char in specials for char in user_password):
-        score += 1
-    else:
-        score += 0
+        
+        score = 0
 
-    #character cases
-    for letter in user_password:
-        if letter.islower():
-            score += 0
-        elif letter.isupper():
-            score +=1
-    
-    #digits
-    if any(user_password.isdigit() for user_password in password):
-        score += 1
+                # check length
+        length = len(user_password)
+        if length > 12:
+                    score += 2
+        elif length >= 8 and length <= 12:
+                    score += 1
+        elif length < 8:
+                    score += 0
+                
+                #special characters
+        specials = "!@#$%^&*()_+{}[]|\\:;\"'<>,.?/~`"
 
-    #usual
-    
+        if any(char in specials for char in user_password):
+                    score += 1
+                
 
-result= passwordstrengthcheck(user_password)
-print(f'pass strength : {result}')
-passwordstrengthcheck()
+                #character cases
+        for letter in user_password:
+                    if letter.islower():
+                        score += 0
+                    elif letter.isupper():
+                        score +=1
+                
+                #digits
+        if any(user_password.isdigit() for user_password in password):
+                    score += 1
+
+                #usual
+        usual = ['root', 'admin',' god', 'master', '123456', '12345', 'guest', 'secret', 'qwerty'] 
+        if user_password in usual:
+                    score += 0
+        elif user_password not in usual:
+                    score +=1
+
+        if score >= 6:
+                    print(strength_label[2])
+        elif score >= 3:
+                    print(strength_label[1])
+        elif score < 3:
+                    print(strength_label[0])
+        result= (user_password)
+        print(f'Your password strength is : {result}')
+
+
+proceed(question, delay = 0.06)
